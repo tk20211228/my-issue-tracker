cc5eecf286ec51059d043f2317d5fb7fab0e54f7
droid-sentry <prod.droidsentry@gmail.com>
2025-06-16 14:45:34 +0900

    feat: add GitHub Actions workflow for deploying static content to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "REPOSITORY_NAME" : "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
